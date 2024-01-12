8e9dc7cfa739a9b17f4ddd493a1732cc8306ac45
Brian Barasa <briannbig@gmail.com>
2024-01-12 21:04:59 +0300

    Update home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CalculatorComponent } from '../components/calculator/calculator.component';
+import { CommonModule } from '@angular/common';
+import { SavingService } from '../core/service/saving.service';
 
 
 @Component({
   selector: 'app-home',
   standalone: true,
+  imports: [CalculatorComponent, CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+
+  
+
+  constructor(private savingsService: SavingService) { }
+  ngOnInit(): void {
+    
+
+  }
+
 
 }
