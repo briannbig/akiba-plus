778aaf1361e4e7728752cd575ffb10f31ed011f4
Brian Barasa <briannbig@gmail.com>
2024-01-13 12:43:47 +0300

    refactor to move home component to views folder

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -1,10 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { CalculatorComponent } from '../components/calculator/calculator.component';
+import { CalculatorComponent } from '../../components/calculator/calculator.component';
 import { CommonModule } from '@angular/common';
-import { SavingCycle } from '../core/models/saving-cycle';
-import { SavingPlanListComponent } from '../components/saving-plan-list/saving-plan-list.component';
-import { SavingService } from '../core/service/saving.service';
-import { SavingPlan } from '../core/models/saving-plan';
+import { SavingCycle } from '../../core/models/saving-cycle';
+import { SavingPlanListComponent } from '../../components/saving-plan-list/saving-plan-list.component';
+import { SavingService } from '../../core/service/saving.service';
+import { SavingPlan } from '../../core/models/saving-plan';
 
 
 @Component({
