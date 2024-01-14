b46dc3f7633a4fc946de427addbf991449e2551d
Brian Barasa <briannbig@gmail.com>
2024-01-14 12:41:09 +0300

    submit login form data to server

--- src/app/components/dialogs/login-dialog/login-dialog.component.ts
+++ src/app/components/dialogs/login-dialog/login-dialog.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 import { MatDialogTitle, MatDialogContent } from '@angular/material/dialog'
 import { AuthService } from '../../../core/service/auth/auth.service';
@@ -14,11 +14,16 @@ import { AuthService } from '../../../core/service/auth/auth.service';
 })
 export class LoginDialogComponent {
 
+  loginForm = new FormGroup({
+    username: new FormControl('', { nonNullable: false }),
+    password: new FormControl('', { nonNullable: false })
+  })
+
 
   constructor(private auth: AuthService) { }
 
   signIn() {
-    this.auth.signIn("", "")
+    this.auth.signIn(this.loginForm.value.username!, this.loginForm.value.password!)
   }
 
 
