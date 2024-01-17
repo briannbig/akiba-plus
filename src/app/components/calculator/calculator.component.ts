a2e8f5e684810157d9ee1919aa79a7ea64deb313
Brian Barasa <briannbig@gmail.com>
2024-01-17 13:36:21 +0300

    replace manual input on calculator component with slider

--- src/app/components/calculator/calculator.component.ts
+++ src/app/components/calculator/calculator.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
-import { FormGroup, FormControl, ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
+import { MatSliderModule } from '@angular/material/slider'
 import { CommonModule } from '@angular/common';
 import { SavingPlan } from '../../core/models/saving-plan';
 import { SavingCycle } from '../../core/models/saving-cycle';
@@ -11,7 +12,7 @@ import { AuthService } from '../../core/service/auth/auth.service';
 @Component({
   selector: 'app-calculator',
   standalone: true,
-  imports: [ReactiveFormsModule, CommonModule],
+  imports: [CommonModule, MatSliderModule, FormsModule],
   templateUrl: './calculator.component.html',
   styleUrl: './calculator.component.css'
 })
@@ -24,16 +25,36 @@ export class CalculatorComponent implements OnInit {
 
   savingPlan: SavingPlan | undefined
   tarrifs = Object.values(SavingCycle)
+
   strategy: SavingStrategy = SavingStrategy.NORMAL
   aggressive: boolean = false
+  savingCycle: SavingCycle = SavingCycle.DAILY
 
-  calcForm = new FormGroup({
-    tarrif: new FormControl(SavingCycle.DAILY, { nonNullable: true }),
-    amount: new FormControl(10, { nonNullable: true }),
-    duration: new FormControl(3, { nonNullable: true }),
-    goal: new FormControl('')
 
-  })
+  showTicks: boolean = true
+  durationSteps: number = 1
+  amountSteps: number = 5
+  thumbLabel: boolean = true
+
+  savingAmount: number = 0
+  duration: number = 0
+
+  maxSavingAmount: number = 10000
+  minSavingAmount: number = 0
+
+  minDuration: number = 1
+  maxDuration: number = 365
+
+
+  goal: string | undefined
+
+  // calcForm = new FormGroup({
+  //   tarrif: new FormControl(SavingCycle.DAILY, { nonNullable: true }),
+  //   amount: new FormControl(10, { nonNullable: true }),
+  //   duration: new FormControl(3, { nonNullable: true }),
+  //   goal: new FormControl('')
+
+  // })
 
   constructor(private calculatorService: CalculatorService, private savingsService: SavingService, private auth: AuthService) { }
   ngOnInit(): void {
@@ -42,16 +63,16 @@ export class CalculatorComponent implements OnInit {
 
 
   calculate() {
-    const duration = this.calcForm.value.duration!;
-    const amount = this.calcForm.value.amount!;
-    const tarrif = this.calcForm.value.tarrif!;
+    const duration = this.duration
+    const amount = this.savingAmount
+    const savingCycle = this.savingCycle
 
     const startdate = new Date()
 
     const target = this.calculatorService.calculateSavingsTargetAmount(this.strategy, duration, amount)
 
     this.savingPlan = {
-      savingCycle: tarrif,
+      savingCycle: savingCycle,
       savingStrategy: this.strategy,
       duration: duration,
       amount: amount,
@@ -65,7 +86,7 @@ export class CalculatorComponent implements OnInit {
   }
 
   createPlan() {
-    this.savingPlan!.goal = this.calcForm.value.goal!
+    this.savingPlan!.goal = this.goal
     this.savingsService.addPlan(this.savingPlan!).subscribe((res: any) => {
       if (res) {
         this.planAdded.emit(res)
